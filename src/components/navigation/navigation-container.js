de3804a130ee57d32018e825619bf57f5a3ac645
Nes'et Kablan <foulofdoom@gmail.com>
2021-06-21 11:13:52 -0600

    nav bar and stuff

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class NavigationContainer extends Component {
+    constructor() {
+        super();
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+            <NavLink exact to ="/">
+                Home
+            </NavLink>
+            <NavLink exact to ="/about">
+                About
+            </NavLink>
+                {false ? <button>Add Blog</button> : null}
+            </div>
+        )
+    }
+}
