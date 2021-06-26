3e44ceb475545708e5658ee72eb69f617e875d17
Nes'et Kablan <foulofdoom@gmail.com>
2021-06-25 22:34:52 -0600

    img fixed, auth nearly done

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"; 
+import Login from "../auth/login";
 import loginImg from "../../../static/assets/images/auth/login.jpeg";
 
 export default class Auth extends Component {
@@ -7,13 +8,13 @@ export default class Auth extends Component {
             <div className="auth-page-wrapper">
                <div className="left-column"
                style={{
-                   backgroundImage: "url(../../../static/assets/images/auth/login.jpeg)"
+                   backgroundImage: `url(${loginImg})`
                }}
                
-               />
+               >???????</div>
 
                <div className="right-column">
-                   <h1>Login Component Goes here</h1>
+                   <Login />
                </div>
             </div> 
         );
