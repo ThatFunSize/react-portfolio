bc59528520e35e2fb699bea6d29a45d07405c00d
Nes'et Kablan <foulofdoom@gmail.com>
2021-07-09 13:12:24 -0600

    inf scroll finished, blog modal start

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'; 
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from 'axios';
 import BlogItem from '../blog/blog-item';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import BlogModal from '../modals/blog-modal';
 
 class Blog extends Component {
     constructor() {
@@ -12,23 +13,36 @@ class Blog extends Component {
             blogItems: [],
             totalCount: 0,
             currentPage: 0,
-            isLoading: true
+            isLoading: true,
+            blogModalIsOpen: false
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfiniteScroll();
+        this.onScroll = this.onScroll.bind(this);
+        window.addEventListener("scroll", this.onScroll, false);
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
     }
 
-    activateInfiniteScroll() {
-        window.onscroll = () => {
-            console.log("window inner height", window.innerHeight)
-            console.log("document sctoll top", document.documentElement.scrollTop)
-            console.log("offset height", document.documentElement.offsetHeight)
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true
+        });
+    }
 
-            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-                console.log("get more posts")
-            }
-        };
+    onScroll() {
+        if (
+            this.state.isLoading || 
+            this.state.blogItems.length === this.state.totalCount
+        ) {
+            return;
+        }
+
+        if (
+            window.innerHeight + document.documentElement.scrollTop === 
+            document.documentElement.offsetHeight
+        ) {
+            this.getBlogItems();
+        }
     }
     
 
@@ -38,12 +52,13 @@ class Blog extends Component {
         })
         axios
             .get(
-                "https://nesetkablan.devcamp.space/portfolio/portfolio_blogs",
+                `https://nesetkablan.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
                 { withCredentials: true }
             )
             .then(response => {
+               console.log("getting", response.data)
                this.setState({
-                   blogItems: response.data.portfolio_blogs,
+                   blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
                    totalCount: response.data.meta.total_records,
                    isLoading: false
                })
@@ -55,6 +70,10 @@ class Blog extends Component {
 
     componentWillMount() {
         this.getBlogItems()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false);
     }
 
     render() {
@@ -71,12 +90,23 @@ class Blog extends Component {
 
         return (
             <div className="blog-container">
-                <div className="content-loader">
-                    <FontAwesomeIcon icon="circle-notch" spin />
+                <BlogModal modalIsOpen={this.state.blogModalIsOpen}/>
+
+                <div className="new-blog-link">
+                    <a onClick={this.handleNewBlogClick}>
+                        Open Modal
+                    </a>
                 </div>
+
                 <div className="content-container">
                     {blogRecords}
                 </div>
+
+                {this.state.isLoading ? (
+                <div className="content-loader">
+                    <FontAwesomeIcon icon="circle-notch" spin className="fast-spin"/>
+                </div>
+                ) : null}  
             </div>
             
 
