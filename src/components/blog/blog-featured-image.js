716f51c7fe06e39f303b7ee791f3fcc08e535446
Nes'et Kablan <foulofdoom@gmail.com>
2021-07-15 11:57:30 -0600

    (NPM I) blog uploader done, working on toolbar

--- src/components/blog/blog-featured-image.js
+++ src/components/blog/blog-featured-image.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function () {
-  return <div></div>;
-}
+const BlogFeaturedImage = (props) => {
+  if (!props.img) {
+    return null;
+  }
+
+  return (
+    <div className="featured-image-wrapper">
+      <img src={props.img} />
+    </div>
+  );
+};
+
+export default BlogFeaturedImage;
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ReactHtmlParser from 'react-html-parser';
+import BlogFeaturedImage from '../blog/blog-featured-image';
 
 export default class BlogDetail extends Component {
   constructor(props) {
@@ -42,11 +43,7 @@ export default class BlogDetail extends Component {
         <div className="content-container">
           <h1>{title}</h1>
 
-          {featured_image_url ? (
-            <div className="featured-image-wrapper">
-              <img src={featured_image_url} />
-            </div>
-          ) : null}
+          <BlogFeaturedImage img={featured_image_url} />
 
           <div className="content">{ReactHtmlParser(content)}</div>
         </div>
